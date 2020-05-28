45750a167c53bdedb40e434ed5bab0da2042032a
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-05-28 13:19:37 +0200

    Count top hits

--- entity-linker.py
+++ entity-linker.py
@@ -1,4 +1,5 @@
 import sqlite3
+from collections import Counter
 
 from elasticsearch import Elasticsearch
 
@@ -61,15 +62,32 @@ class EntityLinker:
                 es.index(index="sentence-sampler-index", id=id, body=doc)
                 es.indices.refresh(index="sentence-sampler-index")
 
+            hit_counter = Counter()
+            hit_counter_right = Counter()
+
             for entity in all_test_contexts:
                 for test_context in all_test_contexts[entity]:
                     print(entity, '#', test_context)
 
                     res = es.search(index="sentence-sampler-index", body={"query": {"match": {'context': test_context}}})
                     print("Got %d Hits:" % res['hits']['total']['value'])
-                    for hit in res['hits']['hits']:
+                    hits = res['hits']['hits']
+                    for hit in hits:
                         print(hit['_score'], hit['_source'])
+                        break
+
+                    if len(hits) > 0:
+                        top_hit = res['hits']['hits'][0]
+                        hit_counter[entity] += 1
+                        if top_hit['_source']['entity'] == entity:
+                            print('+++')
+                            hit_counter_right[entity] += 1
+
                     print()
+
+            for entity in entities:
+                print('{0:30}{1:2} / {2:2}'.format(entity, hit_counter_right[entity], hit_counter[entity]))
+
 
 
 def main():
