d5d181e732d2726395e241cedde1ea884997cf83
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-06-04 23:31:26 +0200

    Major refactor link-extractor.py

--- src/link-extractor.py
+++ src/link-extractor.py
@@ -1,15 +1,31 @@
+import argparse
 import sqlite3
 import wikitextparser as wtp
 
 from collections import defaultdict
 from datetime import datetime
-from src.wikipedia import Wikipedia
+
+from wikipedia import Wikipedia
+
+
+#
+# DEFAULT CONFIGURATION
+#
+
 
 WIKIPEDIA_XML = 'data/enwiki-latest-pages-articles.xml'
-LINKS_SQLITE_DB = 'data/links.db'
+LINKS_DB = 'data/links.db'
+IN_MEMORY = False
+COMMIT_FREQUENCY = 10000
+LIMIT = None
 
 
-def create_links_db(conn):
+#
+# DATABASE FUNCTIONS
+#
+
+
+def create_links_table(conn):
     sql_create_table = '''
         CREATE TABLE links (
             from_doc int,      -- hashed lowercase Wikipedia doc title
@@ -45,46 +61,121 @@ def insert_links(conn, links):
     cursor.close()
 
 
-def main():
-    link_count = 0
-    missing_text_count = 0
+#
+# LINK EXTRACTOR
+#
 
-    redirects = defaultdict(set)
-    redirect_count = 0
 
-    with open(WIKIPEDIA_XML, 'rb') as xml, \
-            sqlite3.connect(LINKS_SQLITE_DB) as conn:
-        
-        create_links_db(conn)
+class LinkExtractor:
+    wikipedia_xml: str
+    links_db: str
+    in_memory: bool
+    commit_frequency: int
+    limit: int
 
-        for page_count, page in enumerate(Wikipedia(xml, tag='page')):
-            if page_count % 1000 == 0:
-                print('{} | COMMIT'.format(datetime.now().strftime('%H:%M:%S')))
-                conn.commit()
+    def __init__(self, wikipedia_xml, links_db, in_memory, commit_frequency, limit):
+        self.wikipedia_xml = wikipedia_xml
+        self.links_db = links_db
+        self.in_memory = in_memory
+        self.commit_frequency = commit_frequency
+        self.limit = limit
 
-            if page_count % 1000 == 0:
-                print('{} | {:,} <page>s | {:,} redirects | {:,} links | {:,} missing text'.format(
-                    datetime.now().strftime("%H:%M:%S"), page_count, redirect_count, link_count, missing_text_count))
+    def run(self):
+        if self.in_memory:
+            self.__run_in_memory()
+        else:
+            self.__run_on_disk()
 
-            from_doc = hash(page['title'][0].lower())
+    def __run_on_disk(self):
+        with sqlite3.connect(self.links_db) as conn_links:
+            create_links_table(conn_links)
+            self.__process_wikipedia(conn_links)
+            print('{} | DONE'.format(datetime.now().strftime('%H:%M:%S')))
 
-            if page['redirect']:
-                to_doc = hash(page['redirect'][0].lower())
-                redirects[from_doc].add(to_doc)
-                redirect_count += 1
+    def __run_in_memory(self):
+        with sqlite3.connect(':memory:') as conn_memory:
+            create_links_table(conn_memory)
+            self.__process_wikipedia(conn_memory)
 
-            elif page['text']:
-                links = wtp.parse(page['text'][0]).wikilinks
-                inserts = [(from_doc, hash(link.title.lower())) for link in links]
-                insert_links(conn, inserts)
-                link_count += len(inserts)
+            print('{} | PERSIST'.format(datetime.now().strftime('%H:%M:%S')))
+            with sqlite3.connect(self.links_db) as conn_links:
+                for line in conn_memory.iterdump():
+                    if line not in ('BEGIN;', 'COMMIT;'):  # let python handle the transactions
+                        conn_links.execute(line)
 
-            else:
-                missing_text_count += 1
+            print('{} | DONE'.format(datetime.now().strftime('%H:%M:%S')))
 
-        conn.commit()
-        print('{} | COMMIT'.format(datetime.now().strftime('%H:%M:%S')))
+    def __process_wikipedia(self, conn):
+        link_count = 0
+        missing_text_count = 0
+
+        redirects = defaultdict(set)
+        redirect_count = 0
+
+        with open(self.wikipedia_xml, 'rb') as wikipedia_xml:
+            for page_count, page in enumerate(Wikipedia(wikipedia_xml, tag='page')):
+
+                if self.limit and page_count > self.limit:
+                    break
+
+                if page_count % self.commit_frequency == 0:
+                    print('{} | COMMIT'.format(datetime.now().strftime('%H:%M:%S')))
+                    conn.commit()
+
+                if page_count % 1000 == 0:
+                    print('{} | {:,} <page>s | {:,} redirects | {:,} links | {:,} missing text'.format(
+                        datetime.now().strftime("%H:%M:%S"), page_count, redirect_count, link_count,
+                        missing_text_count))
+
+                if (page_count + 1) % 100001 == 0:
+                    break
+
+                from_doc = hash(page['title'][0].lower())
+
+                if page['redirect']:
+                    to_doc = hash(page['redirect'][0].lower())
+                    redirects[from_doc].add(to_doc)
+                    redirect_count += 1
+
+                elif page['text']:
+                    links = wtp.parse(page['text'][0]).wikilinks
+                    inserts = [(from_doc, hash(link.title.lower())) for link in links]
+                    insert_links(conn, inserts)
+                    link_count += len(inserts)
+
+                else:
+                    missing_text_count += 1
+
+            conn.commit()
+            print('{} | COMMIT'.format(datetime.now().strftime('%H:%M:%S')))
+
+
+#
+# MAIN
+#
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Create the link graph',
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=40, width=120))
+
+    parser.add_argument('--wikipedia-xml', dest='wikipedia_xml', default=WIKIPEDIA_XML,
+                        help='path to Wikipedia XML (default: "{}")'.format(WIKIPEDIA_XML))
+
+    parser.add_argument('--links-db', dest='links_db', default=LINKS_DB,
+                        help='path to links DB (default: "{}")'.format(LINKS_DB))
+
+    parser.add_argument('--in-memory', dest='in_memory', default=IN_MEMORY, action='store_true',
+                        help='build complete links DB in memory before persisting it (default: {})'.format(IN_MEMORY))
+
+    parser.add_argument('--commit-frequency', dest='commit_frequency', default=COMMIT_FREQUENCY,
+                        help='commit to database every ... pages (default: {})'.format(COMMIT_FREQUENCY))
+
+    parser.add_argument('--limit', dest='limit', default=LIMIT, type=int,
+                        help='terminate after ... pages (default: {})'.format(LIMIT))
+
+    args = parser.parse_args()
+
+    link_extractor = LinkExtractor(args.wikipedia_xml, args.links_db, args.in_memory, args.commit_frequency, args.limit)
+    link_extractor.run()
