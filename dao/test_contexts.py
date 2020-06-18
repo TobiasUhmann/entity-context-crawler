9224f3238fbc4aa229a5e59df71ba183666ef774
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-06-18 02:03:19 +0200

    Implement query-contexts.py

--- dao/test_contexts.py
+++ dao/test_contexts.py
@@ -17,3 +17,46 @@ def insert_context(contexts_conn, entity, context):
     cursor = contexts_conn.cursor()
     cursor.execute(sql, (entity, context))
     cursor.close()
+
+
+def select_distinct_entities(conn, limit):
+    sql = '''
+        SELECT DISTINCT entity
+        FROM contexts
+        ORDER BY RANDOM()
+    '''
+
+    cursor = conn.cursor()
+
+    if limit:
+        sql += 'LIMIT ?'
+        cursor.execute(sql, (limit,))
+    else:
+        cursor.execute(sql)
+
+    rows = cursor.fetchall()
+    cursor.close()
+
+    return [row[0] for row in rows]
+
+
+def select_contexts(conn, entity, limit):
+    sql = '''
+        SELECT entity, context
+        FROM contexts
+        WHERE entity = ?
+        ORDER BY RANDOM()
+    '''
+
+    cursor = conn.cursor()
+
+    if limit:
+        sql += 'LIMIT ?'
+        cursor.execute(sql, (entity, limit))
+    else:
+        cursor.execute(sql, (entity,))
+
+    rows = cursor.fetchall()
+    cursor.close()
+
+    return rows
---
+++ src/query-contexts.py
@@ -0,0 +1,100 @@
+import argparse
+import sqlite3
+
+from collections import defaultdict, Counter
+from elasticsearch import Elasticsearch
+from os.path import isfile
+
+from dao.test_contexts import select_distinct_entities, select_contexts
+
+
+def main():
+    #
+    # Parse args
+    #
+
+    parser = argparse.ArgumentParser(
+        description='Query Elasticsearch index for test contexts',
+        formatter_class=lambda prog: argparse.MetavarTypeHelpFormatter(prog, max_help_position=50, width=120))
+
+    parser.add_argument('index_name', metavar='index-name', type=str,
+                        help='name of input Elasticsearch index')
+
+    parser.add_argument('test contexts_db', metavar='test-contexts-db', type=str,
+                        help='path to input test contexts DB')
+
+    default_limit_contexts = 100
+    parser.add_argument('--limit-contexts', dest='limit_contexts', type=int, default=default_limit_contexts,
+                        help='only process the first ... contexts for each entity'
+                             ' (default: {})'.format(default_limit_contexts))
+
+    default_limit_entities = 10
+    parser.add_argument('--limit-entities', dest='limit_entities', type=int, default=default_limit_entities,
+                        help='only process the first ... entities'
+                             ' (default: {})'.format(default_limit_entities))
+
+    args = parser.parse_args()
+
+    #
+    # Print applied config
+    #
+
+    print('Applied config:')
+    print('    {:20} {}'.format('Index name', args.index_name))
+    print('    {:20} {}'.format('Test contexts DB', args.test_contexts))
+    print()
+    print('    {:20} {}'.format('Limit contexts', args.limit_contexts))
+    print('    {:20} {}'.format('Limit entities', args.limit_entities))
+    print()
+
+    #
+    # Check for input/output files
+    #
+
+    if not isfile(args.test_contexts_db):
+        print('Test contexts DB not found')
+        exit()
+
+    es = Elasticsearch()
+    if not es.indices.exists(index=args.index_name):
+        print('Elasticsearch index not found')
+        exit()
+
+    #
+    # Run program
+    #
+
+    query_contexts(es, args.index_name, args.test_contexts_db, args.limit_contexts, args.limit_entities)
+
+
+def query_contexts(es, index_name, test_contexts_db, limit_contexts, limit_entities):
+    with sqlite3.connect(test_contexts_db) as test_contexts_conn:
+        stats = defaultdict(Counter)
+
+        entities = select_distinct_entities(test_contexts_conn, limit_entities)
+
+        for entity in entities:
+            test_contexts = select_contexts(test_contexts_conn, limit_contexts)
+            for test_context in test_contexts:
+                print(' {:5}  {:24}  {}'.format('QUERY', entity, repr(test_context[:100])))
+                print(100 * '-')
+
+                res = es.search(index=index_name, body={'query': {'match': {'context': test_context}}})
+
+                hits = res['hits']['hits']
+                for hit in hits:
+                    score = hit['_score']
+                    hit_entity = hit['_source']['entity']
+                    concat = repr(hit['_source']['context'][:100])
+                    print(' {:5.1f}  {:24}  {}'.format(score, hit_entity, concat))
+                    stats[entity][hit_entity] += 1
+
+                print()
+
+
+#
+#
+#
+
+if __name__ == '__main__':
+    main()
