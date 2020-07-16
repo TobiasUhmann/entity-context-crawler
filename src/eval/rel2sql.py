74232e476d223e349425425d8b17800ab6a1ac3a
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-07-16 11:36:08 +0200

    Adjust rel2sql scaffold: Use ryn for parsing dataset

--- src/eval/rel2sql.py
+++ src/eval/rel2sql.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import sqlite3
 from argparse import ArgumentParser, HelpFormatter
+from os.path import isdir
+
+from ryn.graphs.split import Dataset
 
 
 def main():
     arg_parser = ArgumentParser(description='Read OpenKE relations txt files into SQLite DB for easier debugging',
                                 formatter_class=lambda prog: HelpFormatter(prog, max_help_position=60, width=120))
 
-    arg_parser.add_argument('relations_txt', metavar='relations-txt',
-                            help='path to input OpenKE relations txt file')
+    arg_parser.add_argument('dataset_dir', metavar='dataset-dir',
+                            help='path to directory containing data files in OpenKE format')
 
     arg_parser.add_argument('sqlite_db', metavar='sqlite-db',
-                            help='path to target SQLite DB (create if not existing)')
-
-    arg_parser.add_argument('table_name', metavar='table-name',
-                            help='name of table in SQLite DB (must not exist yet)')
+                            help='path to output SQLite DB')
 
     args = arg_parser.parse_args()
 
@@ -24,20 +24,36 @@ def main():
     #
 
     print('Applied config:')
-    print('    {:24} {}'.format('Relations txt', args.relations_txt))
+    print('    {:24} {}'.format('Dataset dir', args.dataset_dir))
     print('    {:24} {}'.format('SQLite DB', args.sqlite_db))
-    print('    {:24} {}'.format('Table name', args.table_name))
     print()
+
+    #
+    # Check for input/output files
+    #
+
+    if not isdir(args.dataset_dir):
+        print('Dataset dir not found')
+        exit()
 
     #
     # Fill database
     #
 
-    rel2sql()
+    rel2sql(args.dataset_dir, args.sqlite_db)
 
 
-def rel2sql():
-    print('rel2sql')
+def rel2sql(dataset_dir, sqlite_db):
+    #
+    # Load data
+    #
+
+    print('Read dataset...', end='')
+    dataset = Dataset.load(dataset_dir)
+    print(' done')
+
+    ent2id = dataset.ent2id
+    rel2id = dataset.rel2id
 
 
 if __name__ == '__main__':
