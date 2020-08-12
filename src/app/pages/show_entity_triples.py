ca62130cfd5c3ac67b9cadf77672a99c7327c7ae
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-08-12 11:13:56 +0200

    Add "Closed world entities" option

--- src/app/pages/show_entity_triples.py
+++ src/app/pages/show_entity_triples.py
@@ -7,11 +7,11 @@ from app.util import load_dataset
 
 
 def render_show_entity_triples_page():
-    #
-    # Sidebar
-    #
+    """
+    Render UI specific to "Show entity triples" page
+    """
 
-    st.title('Entity triples')
+    st.title('Show entity triples')
 
     #
     # Load data
@@ -23,6 +23,7 @@ def render_show_entity_triples_page():
     id2ent = dataset.id2ent
     id2rel = dataset.id2rel
 
+    cw_entities: Set[str] = {id2ent[ent] for ent in dataset.cw_train.owe}
     cw_triples = {(id2ent[head], id2ent[tail], id2rel[rel])
                   for head, tail, rel in dataset.cw_train.triples | dataset.cw_valid.triples}
 
@@ -30,7 +31,7 @@ def render_show_entity_triples_page():
     ow_triples = {(id2ent[head], id2ent[tail], id2rel[rel])
                   for head, tail, rel in dataset.ow_valid.triples}
 
-    all_triples = list(cw_triples | ow_triples)
+    all_entities = cw_entities | ow_entities
 
     #
     #
@@ -38,9 +39,23 @@ def render_show_entity_triples_page():
 
     st.sidebar.markdown('---')
 
-    prefix = st.sidebar.text_input('Entity prefix', value='Ab')
+    entity_selection = st.sidebar.selectbox('', [
+        'Closed world entities',
+        'Open world entities',
+        'All entities'
+    ])
 
-    filtered_entities = [entity for entity in ow_entities if entity.startswith(prefix)]
+    prefix = st.sidebar.text_input('Entity prefix', value='Abr')
+
+    if entity_selection == 'Closed world entities':
+        filtered_entities = [ent for ent in cw_entities if ent.startswith(prefix)]
+    elif entity_selection == 'Open world entities':
+        filtered_entities = [ent for ent in ow_entities if ent.startswith(prefix)]
+    elif entity_selection == 'All entities':
+        filtered_entities = [ent for ent in all_entities if ent.startswith(prefix)]
+    else:
+        raise
+
     filtered_entities.sort()
 
     selected_entity = st.sidebar.selectbox('Entity', filtered_entities)
