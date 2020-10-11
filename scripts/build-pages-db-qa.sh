943b6e7b1131ff87c906b26ba27d68c40c0e00d4
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-10-11 22:35:09 +0200

    Update build-pages scripts

--- scripts/build-pages-db-qa.sh
+++ scripts/build-pages-db-qa.sh
@@ -3,5 +3,6 @@
 PYTHONPATH=src/ \
 nohup python -u src/sam.py build-pages-db \
   data/enwiki-20200920.xml \
-  data/pages-v1-enwiki-20200920-qa.db \
+  data/enwiki-2018-09-text.xml \
+  data/pages-v2-enwiki-20200920-enwiki-2018-09-qa.db \
 > log/build-pages-db-qa_$(date +'%Y-%m-%d_%H-%M-%S').stdout &
