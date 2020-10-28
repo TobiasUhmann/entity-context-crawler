136a832bc9f12c2e9036c5825e05cbbf607f3a8e
Tobias Uhmann <tobiasuhmann@gmail.com>
2020-10-29 00:27:58 +0100

    Upgrade contexts DB to v2

--- bash/build-baseline-100-qa.sh
+++ bash/build-baseline-100-qa.sh
@@ -2,9 +2,9 @@
 
 PYTHONPATH=src/ \
 nohup python -u src/sam.py build-baseline \
-  data/contexts-v1-enwiki-20200920-100-500.db \
+  data/contexts-v2-enwiki-20200920-100-500.db \
   data/oke.fb15k237_30061990_50/ \
-  cw-contexts-v1-enwiki-20200920-100-500-qa \
-  data/ow-contexts-v1-enwiki-20200920-100-500-qa.db \
+  cw-contexts-v2-enwiki-20200920-100-500-qa \
+  data/ow-contexts-v2-enwiki-20200920-100-500-qa.db \
   --limit-contexts 100 \
 > log/build-baseline-100-qa_$(date +'%Y-%m-%d_%H-%M-%S').stdout &
--- bash/build-contexts-db.sh
+++ bash/build-contexts-db.sh
@@ -4,9 +4,9 @@ PYTHONPATH=src/ \
 nohup python -u src/sam.py build-contexts-db \
   data/entity2wikidata.json \
   data/matches-v2-enwiki-20200920.db \
-  data/contexts-v1-enwiki-20200920-100-500.db \
+  data/contexts-v2-enwiki-20200920-100-500.db \
   --context-size 500 \
   --crop-sentences \
-  --csv-file data/contexts-v1-enwiki-20200920-100-500.csv \
+  --csv-file data/contexts-v2-enwiki-20200920-100-500.csv \
   --limit-contexts 100 \
 > log/build-contexts-db_$(date +'%Y-%m-%d_%H-%M-%S').stdout &
