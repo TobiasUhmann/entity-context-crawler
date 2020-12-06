298e27f39a592ac5e6eac8910e665a909d281a5d
Tobias Uhmann <tobias.uhmann@gmail.com>
2020-12-06 19:50:41 +0100

    Rewrite pages/predict_entity_triples.py

--- src/app/pages/predict_entity_triples.py
+++ src/app/pages/predict_entity_triples.py
@@ -13,63 +13,49 @@ from models.baseline_model import BaselineModel
 
 
 def render_predict_entity_triples_page():
-    """
-    - Load dataset
-    - Render sidebar
-        - Model independent params
-        - Model dependent params
-    - Render main content
-        - Entity prefix input & Entity name selection
-        - Evaluate model
-        - Show predicted triples
-    """
-
-    dataset = load_dataset()
-
-    id2ent = dataset.id2ent
-    id2rel = dataset.id2rel
-
-    ow_entities: Set[int] = dataset.ow_valid.owe
-    ow_triples: Set = dataset.ow_valid.triples
 
     #
-    # Sidebar: Model independent params
+    # Sidebar: Input model independent params
     #
 
     st.sidebar.markdown('---')
 
-    model_selection = st.sidebar.selectbox('Model', ['Baseline 10', 'Baseline 100'])
-
-    ow_contexts_db = st.sidebar.text_input('OW Contexts DB', value='data/ow-contexts-v7-enwiki-20200920-100-500.db')
-
+    model_selection = st.sidebar.selectbox('Model', ['Baseline'])
+    dataset_dir = st.sidebar.text_input('Dataset directory', 'data/oke.fb15k237_30061990_50')
     random_seed = st.sidebar.number_input('Random seed', value=0)
-    random.seed(random_seed)
 
     st.sidebar.markdown('PYTHONHASHSEED = %s' % os.getenv('PYTHONHASHSEED'))
 
     #
-    # Sidebar: Model dependent params
+    # Sidebar: Input model dependent params
     #
 
     st.sidebar.markdown('---')
 
-    if model_selection == 'Baseline 10':
-        es_url = st.sidebar.text_input('Elasticsearch Host', value='localhost:9200')
-        es = Elasticsearch([es_url])
-        cw_es_index = st.sidebar.text_input('CW Elasticsearch Index', value='cw-contexts-v7-enwiki-20200920-10-500')
-        model = BaselineModel(dataset, es, cw_es_index, ow_contexts_db)
-
-    elif model_selection == 'Baseline 100':
-        es_url = st.sidebar.text_input('Elasticsearch Host', value='localhost:9200')
-        es = Elasticsearch([es_url])
-        cw_es_index = st.sidebar.text_input('CW Elasticsearch Index', value='cw-contexts-v7-enwiki-20200920-100-500')
-        model = BaselineModel(dataset, es, cw_es_index, ow_contexts_db)
+    if model_selection == 'Baseline':
+        es_host = st.sidebar.text_input('Elasticsearch Host', value='localhost:9200')
+        es_index = st.sidebar.text_input('Elasticsearch Index Name', value='cw-contexts-v7-enwiki-20200920-100-500')
+        ow_db = st.sidebar.text_input('Open-World DB', value='data/ow-contexts-v7-enwiki-20200920-100-500.db')
 
     else:
         raise AssertionError()
 
     #
-    # Entity prefix input & Entity name selection
+    # Load dataset & Seed random
+    #
+
+    dataset = load_dataset(dataset_dir)
+
+    id2ent = dataset.id2ent
+    id2rel = dataset.id2rel
+
+    ow_entities = list(dataset.ow_valid.owe)
+    ow_triples = [(head, rel, tail) for head, tail, rel in dataset.ow_valid.triples]
+
+    random.seed(random_seed)
+
+    #
+    # Main: Title & Input entity
     #
 
     st.title('Predict entity triples')
@@ -85,39 +71,33 @@ def render_predict_entity_triples_page():
     selected_entity = int(re.match(regex, selected_option).group(1))  # get number, e.g. 123
 
     #
-    # Evaluate model
+    # Create model & Predict
+    #
+
+    if model_selection == 'Baseline':
+        es = Elasticsearch([es_host])
+        model = BaselineModel(dataset_dir, es, es_index, ow_db)
+        model.calc_score_matrix(ow_entities)
+
+        pred_triples = model.predict(selected_entity)
+
+    else:
+        raise AssertionError()
+
+    #
+    # Main: Output predicted triples
     #
 
     st.markdown('---')
 
-    evaluator = CustomEvaluator(model, ow_triples, [selected_entity])
-    total_result = evaluator.run()
-    result = total_result.results[0]
+    # def highlight(hit):
+    #     if hit == '+':
+    #         return 'color: white; background-color: green'
+    #     elif hit == '-':
+    #         return 'color: white; background-color: red'
+    #     else:
+    #         return 'color: black; background-color: white'
 
-    output = '\n'
-    output += '{:20} {:.2f}'.format('Precision', result.precision) + '\n'
-    output += '{:20} {:.2f}'.format('Recall', result.recall) + '\n'
-    output += '{:20} {:.2f}'.format('F1-Score', result.f1) + '\n'
-    output += '{:20} {:.2f}'.format('Average Precision', result.ap) + '\n'
-    st.markdown('```' + output + '```')
-
-    pred_triples = result.pred_ow_triples
-    pred_triples_hits = result.pred_ow_triples_hits
-
-    def highlight(hit):
-        if hit == '+':
-            return 'color: white; background-color: green'
-        elif hit == '-':
-            return 'color: white; background-color: red'
-        else:
-            return 'color: black; background-color: white'
-
-    data = [(hit, id2ent[t[0]], id2ent[t[1]], id2rel[t[2]])
-            for t, hit in zip(pred_triples, pred_triples_hits)]
-    df = pd.DataFrame(data, columns=['', 'Head', 'Tail', 'Rel'])
-    df = df.style.applymap(highlight)
+    df = pd.DataFrame(pred_triples, columns=['Head', 'Relation', 'Tail'])
+    # df = df.style.applymap(highlight)
     st.dataframe(df)
-
-
-def truncate(text, max_len):
-    return (text[:max_len - 3] + '...') if len(text) > max_len else text
