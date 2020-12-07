295b448d9b2d106010566472fbc418644dd8fee0
Tobias Uhmann <tobias.uhmann@gmail.com>
2020-12-07 22:34:32 +0100

    Add ent by label section

--- src/app/pages/look_up_entities_and_relations.py
+++ src/app/pages/look_up_entities_and_relations.py
@@ -6,9 +6,8 @@ from ryn.graphs import split
 
 
 def render_look_up_entities_and_relations_page():
-
     #
-    # Sidebar input
+    # Sidebar
     #
 
     st.sidebar.header('Params')
@@ -25,21 +24,49 @@ def render_look_up_entities_and_relations_page():
     ent_to_label: Dict[int, str] = dataset.id2ent
     rel_to_label: Dict[int, str] = dataset.id2rel
 
+    ents = ent_to_label.keys()
+
     #
-    # Main input
+    # Main
     #
 
     st.title('Look up entities and relations')
 
+    render_entity_by_id_section(dataset, ent_to_label, ents)
+    render_entity_by_label_section(dataset, ent_to_label, ents)
+
+
+def render_entity_by_id_section(dataset, ent_to_label, ents):
     st.header('Look up entity by ID')
 
-    cols = st.beta_columns(3)
+    cols = st.beta_columns([25, 65, 10])
+    ent = cols[0].number_input('Entity ID', key='ebi-id',
+                               min_value=min(ents), max_value=max(ents), value=min(ents))
+    cols[1].text_input('Entity Label', key='ebi-label', value=ent_to_label[ent])
+    cols[2].text_input('CW/OW', key='ebi-cwow', value=ent_type(dataset, ent))
 
-    ents = ent_to_label.keys()
-    ent = cols[0].number_input('Entity ID', min_value=min(ents), max_value=max(ents), value=min(ents))
 
-    cols[1].text_input('Entity Label', value=ent_to_label[ent])
-    cols[2].text_input('CW/OW', value='OW' if ent in dataset.ow_valid.owe else 'CW')
+def render_entity_by_label_section(dataset, ent_to_label, ents):
+    st.header('Look up entity by label')
+
+    cols = st.beta_columns([15, 60, 15, 10])
+
+    infix = cols[0].text_input('Label contains', key='ebl-infix', value='Ab')
+
+    all_ents_with_labels = [(ent, ent_to_label[ent]) for ent in ents]
+    filtered_ents_with_labels = [(ent, label) for ent, label in all_ents_with_labels if infix in label]
+    filtered_ents_with_labels.sort(key=lambda x: x[1])
+
+    ent = cols[1].selectbox('Entity label', key='ebl-label',
+                            options=[ent for ent, label in filtered_ents_with_labels],
+                            format_func=lambda option: ent_to_label[option])
+
+    cols[2].text_input('Entity ID', key='ebl-id', value=ent)
+    cols[3].text_input('CW/OW', key='ebl-cwow', value=ent_type(dataset, ent))
+
+
+def ent_type(dataset, ent) -> str:
+    return 'OW' if ent in dataset.ow_valid.owe else 'CW'
 
 
 @st.cache(allow_output_mutation=True)
