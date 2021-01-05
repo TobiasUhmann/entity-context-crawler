989283f89153bfab66410ac338deff0b2ba5f356
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-05 02:46:38 +0100

    Fix qid-to-rid test

--- test/dao/test_qid_to_rid_txt.py
+++ test/dao/test_qid_to_rid_txt.py
@@ -5,7 +5,7 @@ from dao.qid_to_rid_txt import load_qid_to_rid
 
 class Test(TestCase):
     def test_load_qid_to_rid(self):
-        qid_to_rid = load_qid_to_rid('test-data/qid-to-rid-v1-test.txt')
+        qid_to_rid = load_qid_to_rid('test_data/qid-to-rid-v1-test.txt')
 
         self.assertEqual(len(qid_to_rid), 3)
         self.assertEqual(qid_to_rid['Q108946'], 0)
