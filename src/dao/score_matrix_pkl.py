fc65b48b3a5ff3092c90a64b746aafb91db0bde9
Tobias Uhmann <tobias.uhmann@gmail.com>
2021-01-17 22:36:52 +0100

    Update dao/score_matrix_pkl.py to v2

--- src/dao/score_matrix_pkl.py
+++ src/dao/score_matrix_pkl.py
@@ -4,10 +4,14 @@ import sparse
 
 
 def save_score_matrix(score_matrix_pkl: str, score_matrix: sparse.COO) -> None:
-    with open(score_matrix_pkl, 'wb') as fh:
-        pickle.dump(score_matrix, fh)
+    data = {'version': 2, 'score_matrix': score_matrix}
+
+    with open(score_matrix_pkl, 'wb') as f:
+        pickle.dump(data, f)
 
 
 def load_score_matrix(score_matrix_pkl: str) -> sparse.COO:
-    with open(score_matrix_pkl, 'rb') as fh:
-        return pickle.load(fh)
+    with open(score_matrix_pkl, 'rb') as f:
+        data = pickle.load(f)
+
+    return data['score_matrix']
